0acecbcb53985cc81abeddcaff206a45542bad41
sofiamoniz <sofiamoniz@ua.pt>
2020-10-22 09:55:07 +0100

    comments, command line fix and memory size

--- clean/SimpleTokenizer.py
+++ clean/SimpleTokenizer.py
@@ -11,19 +11,19 @@ class SimpleTokenizer:
         tokens=[]
         count_tokens=0
 
-        self.received_string=self.replace_non_alpha(self.received_string)
-        self.received_string=self.received_string.lower().split()
+        self.received_string=self.replace_non_alpha(self.received_string) #replace non-alpha chars
+        self.received_string=self.received_string.lower().split() #put all chars to lower
 
         for word in self.received_string:
-            if(len(word)>=3):
+            if(len(word)>=3): #only admit strings with len >=3
                 tokens.append(word)
-                count_tokens=1+count_tokens
+                count_tokens=1+count_tokens #count the tokens read
                 
         return tokens
 
 
-    def replace_non_alpha(self,old_string):
+    def replace_non_alpha(self,old_string): #used to replace non-alpha chars
 
-        new_string = re.sub('[^a-zA-Z]+', ' ', old_string)
+        new_string = re.sub('[^a-zA-Z]+', ' ', old_string) 
 
         return new_string
